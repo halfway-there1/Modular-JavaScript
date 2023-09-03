861e9296983f5031b110f8fdeeb5381d12351b27
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2023-09-04 00:29:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var people = [];
+var template = $('#people-template').html();
+
+$('#peopleModule')
+  .find('button')
+  .on('click', function () {
+    people.push($('#peopleModule').find('input').val());
+
+    // to clear the input
+    $('#peopleModule').find('input').val('');
+
+    // data for mustache template
+    var data = {
+      people: people,
+    };
+    $('#peopleModule').find('ul').html(Mustache.render(template, data));
+  });
+
+$('#peopleModule')
+  .find('ul')
+  .on('click', 'i.del', function (e) {
+    var $remove = $(e.target).closest('li');
+    var i = $('#peopleModule').find('ul').find('li').index($remove);
+
+    $remove.remove();
+
+    people.splice(i, 1);
+  });
