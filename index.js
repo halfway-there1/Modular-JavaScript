900d811312d3a2ef9c71c6600a36eb46ee4b9480
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2023-09-04 08:19:21 +0530

    Convert jQuery to object-literal-pattern

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-var people = [];
-var template = $('#people-template').html();
+(function () {
+  var people = {
+    people: ['Will', 'Laura'],
+    template: $('#people-template').html(),
+    init: function () {
+      this.cacheDom();
+      this.bindEvents();
+      this.render();
+    },
+    cacheDom: function () {
+      this.$el = $('#peopleModule');
+      this.$button = this.$el.find('button');
+      this.$input = this.$el.find('input');
+      this.$ul = this.$el.find('ul');
+      this.template = this.$el.find('#people-template').html();
+    },
+    bindEvents: function () {
+      this.$button.on('click', this.addPerson.bind(this));
+      this.$ul.on('click', 'i.del', this.deletePerson.bind(this));
+    },
+    render: function () {
+      var data = {
+        people: this.people,
+      };
+      this.$ul.html(Mustache.render(this.template, data));
+    },
+    addPerson: function () {
+      this.people.push(this.$input.val());
+      this.render();
+      this.$input.val('');
+    },
+    deletePerson: function (e) {
+      var $remove = $(e.target).closest('li');
+      var i = this.$ul.find('li').index($remove);
 
-$('#peopleModule')
-  .find('button')
-  .on('click', function () {
-    people.push($('#peopleModule').find('input').val());
+      this.people.splice(i, 1);
+      this.render();
+    },
+  };
 
-    // to clear the input
-    $('#peopleModule').find('input').val('');
-
-    // data for mustache template
-    var data = {
-      people: people,
-    };
-    $('#peopleModule').find('ul').html(Mustache.render(template, data));
-  });
-
-$('#peopleModule')
-  .find('ul')
-  .on('click', 'i.del', function (e) {
-    var $remove = $(e.target).closest('li');
-    var i = $('#peopleModule').find('ul').find('li').index($remove);
-
-    $remove.remove();
-
-    people.splice(i, 1);
-  });
+  people.init();
+})();
