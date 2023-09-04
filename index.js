a6c634be974251a2f874ec0f9c3188e7c64b6448
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2023-09-04 08:54:36 +0530

    Convert to revealing-module-pattern

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-(function () {
-  var people = {
-    people: ['Will', 'Laura'],
-    template: $('#people-template').html(),
-    init: function () {
-      this.cacheDom();
-      this.bindEvents();
-      this.render();
-    },
-    cacheDom: function () {
-      this.$el = $('#peopleModule');
-      this.$button = this.$el.find('button');
-      this.$input = this.$el.find('input');
-      this.$ul = this.$el.find('ul');
-      this.template = this.$el.find('#people-template').html();
-    },
-    bindEvents: function () {
-      this.$button.on('click', this.addPerson.bind(this));
-      this.$ul.on('click', 'i.del', this.deletePerson.bind(this));
-    },
-    render: function () {
-      var data = {
-        people: this.people,
-      };
-      this.$ul.html(Mustache.render(this.template, data));
-    },
-    addPerson: function () {
-      this.people.push(this.$input.val());
-      this.render();
-      this.$input.val('');
-    },
-    deletePerson: function (e) {
-      var $remove = $(e.target).closest('li');
-      var i = this.$ul.find('li').index($remove);
+var people = (function () {
+  var people = ['Will', 'Steve'];
 
-      this.people.splice(i, 1);
-      this.render();
-    },
+  // cache DOM
+  var $el = $('#peopleModule');
+  var $button = $el.find('button');
+  var $input = $el.find('input');
+  var $ul = $el.find('ul');
+  var template = $el.find('#people-template').html();
+
+  // bind events
+  $button.on('click', addPerson);
+  $ul.on('click', 'i.del', deletePerson);
+
+  _render();
+
+  function _render() {
+    $ul.html(Mustache.render(template, { people: people }));
+  }
+
+  function addPerson(value) {
+    var name = typeof value === 'string' ? value : $input.val();
+    people.push(name);
+    _render();
+    $input.val('');
+  }
+
+  function deletePerson(event) {
+    var i;
+    if (typeof event === 'number') {
+      i = event;
+    } else {
+      var $remove = $(event.target).closest('li');
+      i = $ul.find('li').index($remove);
+    }
+
+    people.splice(i, 1);
+    _render();
+  }
+
+  return {
+    addPerson: addPerson,
+    deletePerson: deletePerson,
   };
-
-  people.init();
 })();
